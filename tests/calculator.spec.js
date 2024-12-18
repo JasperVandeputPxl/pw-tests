72819b991b4c53b2350802a370088850304dadeb
JasperVandeputPxl <jasper.vandeput@student.pxl.be>
2024-12-18 22:51:29 +0100

    add pw project

---
+++ tests/calculator.spec.js
@@ -0,0 +1,36 @@
+const { test, expect } = require("@playwright/test");
+
+const CALCULATOR_APP_IP = "3.92.191.2";
+
+test("Check if the Add button is present", async ({ page }) => {
+  // Go to target page
+  await page.goto(`http://${CALCULATOR_APP_IP}/`);
+
+  // Get the button with text 'Add'
+  const addButton = await page.locator('button', { hasText: 'Add' });
+
+  // Assert that the button is visible
+  await expect(addButton).toBeVisible();
+});
+
+test("Check if the subtract logic works", async ({page}) => {
+  // Go to target page
+  await page.goto(`http://${CALCULATOR_APP_IP}/`);
+
+  // Get the button with text 'Subtract'
+  const subtractButton = await page.locator('button', { hasText: 'Subtract' });
+
+  // Fill the two input fields
+  await page.locator('id=number1').fill('7');
+  await page.locator('id=number2').fill('3');
+
+  // Assert that the button is visible
+  await expect(subtractButton).toBeVisible();
+
+  // Click the button
+  await subtractButton.click();
+
+  // assert that the result is '4'
+  const resultField = await page.locator('id=result');
+  await expect(resultField).toHaveText("The result is: 4");
+});
